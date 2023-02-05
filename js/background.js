97f9a5ec4b517b7d2c6bace04fc898fe42886e90
hwseo12 <59567133+hwseo12@users.noreply.github.com>
2023-02-06 01:52:47 +0900

    commit

---
+++ js/background.js
@@ -0,0 +1,6 @@
+const images = ["0.png", "1.png", "2.png"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+document.body.style.backgroundImage = `url("img/${chosenImage}")`;
+console.log(document.body);
